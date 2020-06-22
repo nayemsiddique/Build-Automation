3a4f6b0afea0b542e68e37d852a2978e119397a8
Nayem Siddique <nayemsiddiqueasif@gmail.com>
2020-06-22 10:30:41 +0600

    'update'

--- git push automation.py
+++ git push automation.py
@@ -1,8 +1,10 @@
 import sys
 import os
+import datetime
 
-f= open("guru99.txt","w+")
-f.close() 
+x = datetime.datetime.now()
+#f= open("demoFile"+str(x)+".txt","w+")
+#f.close() 
 os.system("git add .")
 os.system("git commit -m 'update'")
 os.system("git push")
