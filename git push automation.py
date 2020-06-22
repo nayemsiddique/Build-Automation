7d202435664598f0c4ab10f71fc2517b5bf57e02
Nayem Siddique <nayemsiddiqueasif@gmail.com>
2020-06-22 11:17:17 +0600

    'update'

--- git push automation.py
+++ git push automation.py
@@ -1,10 +1,22 @@
 import sys
 import os
 import datetime
+import msvcrt as m
 
 x = datetime.datetime.now()
-#f= open("demoFile"+str(x)+".txt","w+")
-#f.close() 
+y=str(x).replace(" ", "")
+p="file"
+q=".txt"
+fileName=p+y+q
+print(fileName)
+
+file="file.txt"
+
+f= open(file,"w+")
+m.getch()
+f.close() 
 os.system("git add .")
 os.system("git commit -m 'update'")
 os.system("git push")
+
+m.getch()
